c7fa974c9bca48200154becdfc8ec79c9033222b
zsbd-zsbd <1945679878@qq.com>
2024-08-29 15:25:39 +0800

    Add files via upload

---
+++ regression.sh
@@ -0,0 +1,12 @@
+make clean
+
+make comp 
+
+for((loop_num=0; loop_num < 1 ;loop_num++)) do
+  make sim xxxx_test1 &
+  make sim xxxx_test1 &
+  make sim xxxx_test1 &
+  make sim xxxx_test1 &
+  make sim xxxx_test1 &
+  wait
+done
